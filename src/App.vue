a1ac48f61e99afe1894140e25a8c7cb0ad99c70a
andrea-livornese <andrealivornese@gmail.com>
2024-03-06 17:49:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+import {store} from "../src/store";
+
+export default{
+
+  data(){
+    return{
+      store,
+    }
+  }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
