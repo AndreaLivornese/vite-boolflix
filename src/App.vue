6e3163b8451a8bceed669c33df53260d91db4e73
andrea-livornese <andrealivornese@gmail.com>
2024-03-06 18:39:59 +0100

    fine Milestone 0

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,15 @@ export default{
   methods:{
 
     searchFilm(){
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=39fd68787b3ecc4c7f99e2428228f89c&query=${store.search}`).then((res)=>{
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=39fd68787b3ecc4c7f99e2428228f89c&language=en-US&query=${store.search}`).then((res)=>{
         console.log(res);
+
+        store.films = res.data.results;
+
+        console.log(store.films);
       });
+
+      store.search='';
     }
 
   }
